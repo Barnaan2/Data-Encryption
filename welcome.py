4e0ac84618c8463433352fd0ed45c417fe3cade6
Bernabas Tekkalign <95656020+Barnaan2@users.noreply.github.com>
2022-05-10 23:39:55 +0300

    Create welcome.py

---
+++ welcome.py
@@ -0,0 +1,11 @@
+#To welcome new peoples who join this repo
+print('Hello there i am to work with you')
+name = input('Enter your Name')
+sex = input('Enter your Sex')
+userName= input('Enter your Github user name')
+Campus = input('Enter your campus')
+
+if(Campus == 'Jit'):
+  print('we are in the same campus and i am glad to know it')
+  else:
+ print('glad you contacted me')
